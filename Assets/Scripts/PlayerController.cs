d9b59182ac17210ab03c2140e4329cfc483c3bc2
reisuj <reisuj@gmail.com>
2024-04-28 10:26:35 -0400

    Added Player movement and jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float _jumpForce;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void FixedUpdate()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * _moveSpeed, rb.velocity.y);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
